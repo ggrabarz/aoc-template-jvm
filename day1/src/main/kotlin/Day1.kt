cab86d9a1f8e81629fe94b869d17a160c40f2bc8
Grzegorz Grabarz <grzegorz.grabarz+dev@gmail.com>
2024-12-01 19:58:23 +0100

    AoC day1

--- day1/src/main/kotlin/Day1.kt
+++ day1/src/main/kotlin/Day1.kt
@@ -1,19 +1,49 @@
+import java.util.SortedMap
+import kotlin.math.abs
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
+    val leftLocationIDs = sortedMapOf<Int, Int>()
+    val rightLocationIDs = sortedMapOf<Int, Int>()
 
+    input.lines().forEach { line ->
+        val (left, right) = line.split("   ")
+        leftLocationIDs.put(left.toInt(), leftLocationIDs.computeIfAbsent(left.toInt()) { t -> 0 } + 1)
+        rightLocationIDs.put(right.toInt(), rightLocationIDs.computeIfAbsent(right.toInt()) { t -> 0 } + 1)
+    }
+
+    val expandedLeftLocationIDs = expand(leftLocationIDs)
+    val expandedRightLocationIDs = expand(rightLocationIDs)
+    val zippedLocations = expandedLeftLocationIDs.zip(expandedRightLocationIDs)
+    val distance = zippedLocations.map { (a, b) -> abs(a - b) }.reduce { acc, diff -> acc + diff }
+
+    return distance
+}
+
+fun expand(map: SortedMap<Int, Int>): Sequence<Int> {
+    return map.asSequence().flatMap { (key, value) -> generateSequence { key }.take(value) }
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val leftLocationIDs = mutableListOf<Int>()
+    val rightLocationIDs = hashMapOf<Int, Int>()
+
+    input.lines().forEach { line ->
+        val (left, right) = line.split("   ")
+        leftLocationIDs.add(left.toInt())
+        rightLocationIDs.put(right.toInt(), rightLocationIDs.computeIfAbsent(right.toInt()) { t -> 0 } + 1)
+    }
+
+    return leftLocationIDs.fold(0) { acc, left -> acc + left * rightLocationIDs.getOrDefault(left, 0) }
 }
 
-private val input: String by lazy {readInput()}
+private val input: String by lazy { readInput() }
 
 private fun readInput(): String {
     return object {}.javaClass.getResource("Day1.input").readText()
 }
+
